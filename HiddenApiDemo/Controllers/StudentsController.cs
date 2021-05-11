4dcdba878a560b6377ef0320be7cc7d4aa6649f6
Hassan Habib <hassanhabib@live.com>
2021-05-11 02:18:36 -0700

    Add project files.

---
+++ HiddenApiDemo/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HiddenApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Here's my students");
+
+        [HttpDelete]
+        public ActionResult<string> Delete() =>
+            Ok("Deleted All Students!!!");
+    }
+}
